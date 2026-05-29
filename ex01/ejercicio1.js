9f6fc6b3ced2ccd5e3e7e14492f1bb8ab256d203
Lópezandresdavid340 <escobarlopezandresdavid85@gmail.com>
2026-05-28 20:18:17 -0500

    repositorio

---
+++ ex01/ejercicio1.js
@@ -0,0 +1,9 @@
+/*
+Esto sirve para realizar comentarios en el codigo, es decir, para escribir texto que no se ejecutará como código.
+Los comentarios son útiles para explicar el propósito de ciertas partes del código, hacer anotaciones o dejar recordatorios para otros desarrolladores (o para ti mismo en el futuro).
+*/
+
+// Este es un comentario de una sola línea, se utiliza para escribir una breve explicación o anotación en una sola línea de código.
+
+document.write("Hola Mundo"); 
+console.log("hola desde la consola");
